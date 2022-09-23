336615b24c912df936b253799439f6592de39f0d
xpzhang <XP17125@163.com>
2022-09-23 15:33:01 +0800

    class1

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# DIP_ws
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: xp.Zhang
+ * @Date: 2022-09-23 14:44:55
+ * @LastEditors: xp.Zhang
+ * @LastEditTime: 2022-09-23 15:32:37
+-->
--- helloWorld.py
+++ helloWorld.py
@@ -4,6 +4,15 @@ version:
 Author: xp.Zhang
 Date: 2022-09-23 14:45:22
 LastEditors: xp.Zhang
-LastEditTime: 2022-09-23 14:45:35
+LastEditTime: 2022-09-23 15:32:23
 '''
+
 print("你好！")
+print (5 ** 4)
+print (5 / 3)#!算出是浮点型，与c嘎嘎不同
+print (5 // 3)#向下舍
+print (5 // 3.)
+""" 
+Hello, This is a note.
+"""
+#God dam, You look so great!
